1d0476c13a90e7f033f98d58ac38055fdaa6c704
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2024-06-15 19:16:17 +0300

    ChronicPains

---
+++ Source/Genes/Gene_ChronicPains.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace BMT_PollutedLands
+{
+
+	public class Gene_ChronicPains : Gene
+	{
+
+		public GeneExtension Props => def.GetModExtension<GeneExtension>();
+
+		public int nextTick = -1;
+
+		public override void PostAdd()
+		{
+			base.PostAdd();
+			ResetNextTick();
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+			nextTick--;
+			if (nextTick > 0)
+			{
+				return;
+			}
+			Pain();
+			ResetNextTick();
+		}
+
+		public void ResetNextTick()
+		{
+			nextTick = Props.intervals.RandomInRange;
+		}
+
+		public void Pain()
+		{
+			HediffUtility.TryAddOrRemoveHediff(Props.hediffDef, pawn, this, Props.bodyparts, true);
+		}
+
+		public override IEnumerable<Gizmo> GetGizmos()
+		{
+			if (DebugSettings.ShowDevGizmos)
+			{
+				yield return new Command_Action
+				{
+					defaultLabel = "DEV: ChronicPains",
+					action = delegate
+					{
+						Pain();
+					}
+				};
+			}
+		}
+
+	}
+
+}
