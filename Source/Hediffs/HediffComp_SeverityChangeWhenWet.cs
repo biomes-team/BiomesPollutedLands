ea2b8d44d1cd7ac1c835d49fa289deb6295bf27a
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-08-11 19:54:58 +0200

    added HediffCompProperties_SeverityChangeWhenWet to BMT_Slimed

---
+++ Source/Hediffs/HediffComp_SeverityChangeWhenWet.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+
+namespace BMT_PollutedLands;
+
+public class HediffComp_SeverityChangeWhenWet : HediffComp_SeverityModifierBase
+{
+    private HediffCompProperties_SeverityChangeWhenWet Props =>
+        (HediffCompProperties_SeverityChangeWhenWet)props;
+
+    public override float SeverityChangePerDay()
+    {
+        var changePerDay = Props.baseChangePerDay;
+
+        if (Pawn.IsWet())
+        {
+            changePerDay += Props.isWetChangePerDay;
+        }
+
+        return changePerDay;
+    }
+}
+
+public class HediffCompProperties_SeverityChangeWhenWet : HediffCompProperties
+{
+    public float isWetChangePerDay = -1f;
+    public float baseChangePerDay = 0;
+
+
+    public HediffCompProperties_SeverityChangeWhenWet()
+    {
+        compClass = typeof(HediffComp_SeverityChangeWhenWet);
+    }
+}
---
+++ Source/Utility/WetUtility.cs
@@ -0,0 +1,37 @@
+using RimWorld;
+using Verse;
+
+namespace BMT_PollutedLands;
+
+public static class WetUtility
+{
+    public static bool IsWet(this Pawn pawn)
+    {
+        return OnWater(pawn) || InRain(pawn) || IsSoakingWet(pawn);
+    }
+
+    private static bool IsSoakingWet(Pawn pawn)
+    {
+        return pawn.needs.mood.thoughts.memories?.GetFirstMemoryOfDef(ThoughtDefOf
+            .SoakingWet) != null;
+    }
+
+    public static bool InRain(this Pawn pawn)
+    {
+        return pawn.Map != null && pawn.Position.GetTerrain(pawn.Map) != null &&
+               !pawn.Position.Roofed(pawn.Map) &&
+               pawn.Map.weatherManager.RainRate > 0.01f;
+    }
+
+    public static bool OnWater(this Pawn pawn)
+    {
+        return pawn.Map != null && pawn.Position.GetTerrain(pawn.Map) != null &&
+               pawn.Position.GetTerrain(pawn.Map).IsWater;
+    }
+
+    public static bool InRiver(this Pawn pawn)
+    {
+        return pawn.Map != null && pawn.Position.GetTerrain(pawn.Map) != null &&
+               pawn.Position.GetTerrain(pawn.Map).IsRiver;
+    }
+}
