a5674e32d41baa56c72ba75dfbe656421fa3e0b3
nimragiscoming <nine.seven.ninete@gmail.com>
2024-12-14 18:21:55 +0000

    Bunker Bug Arrives event added

---
+++ Source/IncidentWorker_BunkerBugPasses.cs
@@ -0,0 +1,49 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace BMT_PollutedLands
+{
+    internal class IncidentWorker_BunkerBugPasses : IncidentWorker
+    {
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            Map target = (Map)parms.target;
+            
+            return target.pollutionGrid.TotalPollutionPercent > 0.7f && !target.gameConditionManager.ConditionIsActive(GameConditionDefOf.ToxicFallout) && (!ModsConfig.BiotechActive || !target.gameConditionManager.ConditionIsActive(GameConditionDefOf.NoxiousHaze)) && target.mapTemperature.SeasonAndOutdoorTemperatureAcceptableFor(ThingDef.Named("BMT_BunkerBug")) && this.TryFindEntryCell(target, out IntVec3 _);
+        }
+
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            Map target = (Map)parms.target;
+            IntVec3 cell;
+            if (!this.TryFindEntryCell(target, out cell))
+                return false;
+            PawnKindDef bunkerBug = PawnKindDef.Named("BMT_BunkerBug");
+            int num1 = Mathf.Clamp(GenMath.RoundRandom(StorytellerUtility.DefaultThreatPointsNow((IIncidentTarget)target) / bunkerBug.combatPower), 2, Rand.RangeInclusive(3, 6));
+            int num2 = Rand.RangeInclusive(90000, 150000);
+            IntVec3 result = IntVec3.Invalid;
+            if (!RCellFinder.TryFindRandomCellOutsideColonyNearTheCenterOfTheMap(cell, target, 10f, out result))
+                result = IntVec3.Invalid;
+            Pawn newThing = (Pawn)null;
+            for (int index = 0; index < num1; ++index)
+            {
+                IntVec3 loc = CellFinder.RandomClosewalkCellNear(cell, target, 10);
+                newThing = PawnGenerator.GeneratePawn(bunkerBug);
+                GenSpawn.Spawn((Thing)newThing, loc, target, Rot4.Random);
+                newThing.mindState.exitMapAfterTick = Find.TickManager.TicksGame + num2;
+                if (result.IsValid)
+                    newThing.mindState.forcedGotoPosition = CellFinder.RandomClosewalkCellNear(result, target, 10);
+            }
+            this.SendStandardLetter("BMT_LetterLabelBunkerBugPasses".Translate((NamedArgument)bunkerBug.label).CapitalizeFirst(), "BMT_LetterBunkerBugPasses".Translate((NamedArgument)bunkerBug.label), LetterDefOf.PositiveEvent, parms, (LookTargets)(Thing)newThing);
+            return true;
+        }
+
+        private bool TryFindEntryCell(Map map, out IntVec3 cell) => RCellFinder.TryFindRandomPawnEntryCell(out cell, map, CellFinder.EdgeRoadChance_Animal + 0.2f);
+    }
+}
