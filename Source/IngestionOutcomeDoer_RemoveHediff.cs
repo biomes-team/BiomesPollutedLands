3a8a9a1ce169d69ee4e869cac9ef069ee87b48da
nimragiscoming <nine.seven.ninete@gmail.com>
2024-12-14 19:09:21 +0000

    added ToxClear which removes toxic buildup

---
+++ Source/IngestionOutcomeDoer_RemoveHediff.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BMT_PollutedLands
+{
+    internal class IngestionOutcomeDoer_RemoveHediff : IngestionOutcomeDoer
+    {
+        public HediffDef hediffDef;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            HediffUtility.TryRemoveHediff(hediffDef, pawn);
+        }
+
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats(ThingDef parentDef)
+        {
+            IngestionOutcomeDoer_RemoveHediff outcomeDoerGiveHediff = this;
+            if (parentDef.IsDrug && (double)outcomeDoerGiveHediff.chance >= 1.0)
+            {
+                foreach (StatDrawEntry specialDisplayStat in outcomeDoerGiveHediff.hediffDef.SpecialDisplayStats(StatRequest.ForEmpty()))
+                    yield return specialDisplayStat;
+            }
+        }
+        
+    }
+}
