9f2ed20430802f473f981d57af1ab76bbad0f821
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2024-12-18 03:26:05 +0300

    Give Hediff With Stat

---
+++ Source/IngestionOutcomeDoer_GiveHediff.cs
@@ -0,0 +1,31 @@
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace BMT_PollutedLands
+{
+    public class IngestionOutcomeDoer_GiveHediffWithStat : IngestionOutcomeDoer_GiveHediff
+    {
+
+        public StatDef statDef;
+
+        public bool divideByBodySize = true;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            Hediff hediff = HediffMaker.MakeHediff(hediffDef, pawn);
+            float effect = ((!(severity > 0f)) ? hediffDef.initialSeverity : severity);
+            if (statDef != null)
+            {
+                effect *= pawn.GetStatValue(statDef);
+            }
+            AddictionUtility.ModifyChemicalEffectForToleranceAndBodySize_NewTemp(pawn, toleranceChemical, ref effect, multiplyByGeneToleranceFactors, divideByBodySize);
+            hediff.Severity = effect;
+            pawn.health.AddHediff(hediff);
+        }
+
+    }
+
+}
--- Source/ModExtension.cs
+++ Source/ModExtension.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Verse;
 
-// namespace WVC
 namespace BMT_PollutedLands
 {
 
