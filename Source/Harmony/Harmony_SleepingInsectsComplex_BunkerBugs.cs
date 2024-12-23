7d04c36e9d03a4fe43d8a29136769f93d0ffb06e
nimrag-b <124409677+nimrag-b@users.noreply.github.com>
2024-12-23 16:16:01 +0000

    patched bunker bugs out of spawning in ancient complexed

---
+++ Source/Harmony/Harmony_SleepingInsectsComplex_BunkerBugs.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BMT_PollutedLands
+{
+    [HarmonyPatch(typeof(ComplexThreatWorker_SleepingInsects), "GetPawnKindsForPoints")]
+    public class Harmony_SleepingInsectsComplex_BunkerBugs
+    {
+        public static IEnumerable<PawnKindDef> Postfix(IEnumerable<PawnKindDef> values)
+        {
+            foreach (PawnKindDef p in values)
+            {
+                if(!p.race.HasComp<CompDisableComplexInsectSpawn>())
+                    yield return p;
+            }
+        }
+    }
+
+
+}
