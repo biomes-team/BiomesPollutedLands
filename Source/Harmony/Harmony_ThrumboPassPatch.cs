7d03044c29c613e722bd338b3f83fd98d3c2cdb8
nimrag-b <124409677+nimrag-b@users.noreply.github.com>
2024-12-26 00:45:40 +0000

    Update Harmony_ThrumboPassPatch.cs

--- Source/Harmony/Harmony_ThrumboPassPatch.cs
+++ Source/Harmony/Harmony_ThrumboPassPatch.cs
@@ -7,15 +7,14 @@ namespace BMT_PollutedLands
     [HarmonyPatch(typeof(IncidentWorker), nameof(IncidentWorker.ChanceFactorNow))]
     public class Harmony_ThrumboPassPatch
     {
-        public static bool Prefix(IncidentWorker __instance, IIncidentTarget target, ref float __result)
+        public static void Postfix(IncidentWorker __instance, IIncidentTarget target, ref float __result)
         {
             if(__instance is IncidentWorker_ThrumboPasses)
             {
                 Map map = (Map)target;
                 __result *= 1 - map.pollutionGrid.TotalPollutionPercent;
-                return true;
+
             }
-            return false;
         }
     }
 }
